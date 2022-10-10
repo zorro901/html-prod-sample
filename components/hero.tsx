aa84bb15fa39cdd409cc1a269958ec9799c3efdd
zorro901 <zoro907@gmail.com>
2022-10-10 22:55:35 +0900

    Create Hero

---
+++ components/hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <>
+      <img style={{ height: '606px', objectFit: "cover" }} src="/assets/mainvisual.jpg" alt="" height={606}
+           width={1920} />
+    </>
+  );
+}
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 import Header from "../components/header";
+import Hero from "../components/hero";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className={'h-[200vh]'}>
       <Header />
-    </>
-  )
+      <main>
+        <Hero/>
+      </main>
+      <div className={'h-3'}>footer</div>
+    </div>
+  );
 }
 
 export default Home
