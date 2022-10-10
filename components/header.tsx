1e29c6a0a12deeaac81d88cb504b8ec50e4c6819
zorro901 <zoro907@gmail.com>
2022-10-10 22:05:20 +0900

    Create Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
-    <header>
-      <div></div>
-      <ul>
-        <li>About</li>
-        <li>Bicycle</li>
+    <header className={'flex px-[37px] justify-between items-center'}>
+      <div>
+        <Image src={'/assets/logo.svg'} width={120} height={60}/>
+      </div>
+      <ul className={'flex text-[14.4px] font-meiryo'}>
+        <li className={'ml-[30px]'}>About</li>
+        <li className={'ml-[30px]'}>Bicycle</li>
       </ul>
     </header>
   );
