9600934f6c2bcea5471176f9ecbab921db6a324f
zorro901 <zoro907@gmail.com>
2022-10-10 21:50:09 +0900

    Init

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div></div>
+      <ul>
+        <li>About</li>
+        <li>Bicycle</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
